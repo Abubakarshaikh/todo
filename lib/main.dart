8a359ee96e28a48f21365ef6090643a2ce090e10
abubakarshaikh <atifa2019@gmail.com>
2021-04-10 00:24:58 -0700

    added appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      appBar: AppBar(
+        title: Text('Mater branch'),
+      ),
       body: Text('Hello, master'),
     ),
   ));
