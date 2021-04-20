f27905819f35a986e386eaf1b57808c12fc314e5
abubakarshaikh <atifa2019@gmail.com>
2021-04-20 03:30:16 -0700

    minner changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo/controller/task_controller.dart';
-import 'package:todo/view/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
